6d47ecbfce2bdf067635967a289ea01906f330ff
ziga <ziga.kovacic03@gmail.com>
2021-09-17 18:36:24 +0200

    first

---
+++ temp/basic.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+#define IO(NAME) \
+    cin.tie(0)->sync_with_stdio(0); \
+    if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+    freopen(NAME ".out","w",stdout); 
+    
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T, class U = null_type, class chash = hash<T>> using hset = 
+gp_hash_table<T,U,chash>;
+template<class T, class U = null_type, class cmp = less<T>> using oset = 
+tree<T,U,cmp,rb_tree_tag,tree_order_statistics_node_update>;
+
+int main() {
+    IO(""); // put the name here
+    // put code here
+}
